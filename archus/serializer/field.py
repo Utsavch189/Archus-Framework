22078725755d561a8476424981b5ddc112d24357
utsav <utsavpokemon9000chatterjee@gmail.com>
2024-06-30 19:17:49 +0530

    update serializer field

--- archus/serializer/field.py
+++ archus/serializer/field.py
@@ -1,11 +1,12 @@
 from archus.serializer.validation_error import ValidationError
 
 class Field:
-    def __init__(self, required=False, data_type=None, default=None,validator=None):
+    def __init__(self, required=False, data_type=None, default=None,validator=None,external_serializer=None):
         self.required = required
         self.data_type = data_type
         self.default = default
         self.validator=validator
+        self.external_serializer=external_serializer
 
     def validate(self, value, field_name,partial=False):
         if not partial:
@@ -13,6 +14,13 @@ class Field:
                 raise ValidationError(f"The field '{field_name}' is required.",field_name)
         if self.data_type and value is not None and not isinstance(value, self.data_type):
             raise ValidationError(f"Invalid data type for field '{field_name}'. Expected {self.data_type.__name__}.", field_name)
+
+        if self.external_serializer and self.data_type==dict:
+            self.external_serializer().validated_data(value)
+        
+        if self.external_serializer and self.data_type==list:
+            for data in value:
+                self.external_serializer().validated_data(data)
 
         if self.validator and value is not None:
             value=self.validator(value, field_name)
